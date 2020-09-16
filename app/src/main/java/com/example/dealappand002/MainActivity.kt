af1fbf0f7ebfb2324ee17e3f64fdb6198e0d0084
leejeeyong <fghj0720@gmail.com>
2020-09-16 16:56:51 +0900

    로그인 기능

--- app/src/main/java/com/example/dealappand002/MainActivity.kt
+++ app/src/main/java/com/example/dealappand002/MainActivity.kt
@@ -4,24 +4,49 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.ImageView
 import android.widget.TextView
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
+        // Access a Cloud Firestore instance from your Activity
+        val db = FirebaseFirestore.getInstance()
+
 
         ButtonLoginAction.setOnClickListener {
-            var intent = Intent(this,MainPage::class.java)
-            startActivity(intent)
+
+            db.collection("UserData")
+                .get()
+                .addOnSuccessListener { result ->
+                    for (document in result) {
+                        if(editTextID.text.toString() == document.getString("ID")){
+                            Log.d(TAG,"아이디 잉치")
+                            if(editTextPassword.text.toString() == document.getString("PASSWORD")){
+                                Log.d(TAG,"비밀번호 잉치")
+                                var intent = Intent(this,MainPage::class.java)
+                                startActivity(intent)
+                            }
+                        }
+                    }
+                }
+                .addOnFailureListener { exception ->
+                    Log.w(TAG, "Error getting documents.", exception)
+                }
+
         }
     }
 
